be28d3a5017c8714cba1d207caf28dd8fa06f19e
Jhonattan David Orjuela Mora <jhonattan0925@gmail.com>
2025-07-06 23:47:33 -0500

    Se crea el nuevo proyecto client para el consumo del apirest

---
+++ spring-boot-webflux-client/src/main/java/com/learning/webflux/app/models/Product.java
@@ -0,0 +1,61 @@
+package com.learning.webflux.app.models;
+
+import java.time.LocalDateTime;
+
+public class Product {
+
+    private String id;
+    private String name;
+    private Double price;
+    private LocalDateTime createAt;
+    private Category category;
+    private String image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
